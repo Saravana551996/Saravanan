bbbbecde39652799f6bb580fd703798d5d15a954
Saravana551996 <ksd.saravanan8@gmail.com>
2021-11-12 11:00:07 +0530

    First Time Pushing the code

---
+++ src/test/java/com/project/Class1.java
@@ -0,0 +1,20 @@
+package com.project;
+
+public class Class1 {
+	
+	public void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void method3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

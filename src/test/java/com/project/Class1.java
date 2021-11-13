af3d2de9ff02c0860e9c9903736bd8824dc66104
Saravana551996 <ksd.saravanan8@gmail.com>
2021-11-13 11:01:35 +0530

    Latest COde

--- src/test/java/com/project/Class1.java
+++ src/test/java/com/project/Class1.java
@@ -16,5 +16,19 @@ public class Class1 {
 		// TODO Auto-generated method stub
 
 	}
+	
+	private void method4() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method5() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method6() {
+		// TODO Auto-generated method stub
+
+	}
+	
 
 }
